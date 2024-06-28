c90c7c47a4fa38ec86e78e4b2e56fa6396e5762e
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-06-28 08:45:05 +0300

    Loops

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// function writeCards(names,event){
+function writeCards(names, eventNam) {
+  const Arr = [];
+  for (let i = 0; i < names.length; i++) {
+    Arr.push(`Thank you, ${names[i]}, for the wonderful ${eventNam} gift!`);
+  }
+  return Arr;
+}
+
+function countDown(number) {
+  if (number < 1) {
+    console.log("Please provide a positive integer.");
+    return;
+  }
+
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
